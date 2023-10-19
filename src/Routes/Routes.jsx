4b1d4e0aa7653563752b4ccd0ed51ca470b005d5
Be3tle <aburatinshoaib@gmail.com>
2023-10-20 00:03:37 +0600

    login/reg added

--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -3,6 +3,8 @@ import { createBrowserRouter } from 'react-router-dom';
 import Root from '../Layout/Root';
 import Error from '../Pages/Error/Error';
 import Home from '../Pages/Home/Home';
+import Login from '../Pages/Login/Login';
+import Register from '../Pages/Login/Register';
 
 const routes = createBrowserRouter([
   {
@@ -14,6 +16,14 @@ const routes = createBrowserRouter([
         path: '/',
         element: <Home></Home>,
         loader: () => fetch('data.json'),
+      },
+      {
+        path: '/login',
+        element: <Login></Login>,
+      },
+      {
+        path: '/register',
+        element: <Register></Register>,
       },
     ],
   },
