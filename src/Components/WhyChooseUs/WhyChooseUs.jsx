1b6f7bf2c6a2eb4fc12ab0e3ea1cf8b17498f863
Abu Ratin Shoeb <aburatinshoaib@gmail.com>
2024-01-06 22:48:15 +0600

    styling changed in why choose us

--- src/Components/WhyChooseUs/WhyChooseUs.jsx
+++ src/Components/WhyChooseUs/WhyChooseUs.jsx
@@ -1,48 +1,56 @@
+import { TbBrandApple } from 'react-icons/tb';
+import { FaShippingFast } from 'react-icons/fa';
+import { BiBadgeCheck } from 'react-icons/bi';
+
 const WhyChooseUs = () => {
   return (
-    <section className="why-choose-us py-12">
-      <div className="container mx-auto text-center dark:text-white">
-        <h2 className="section-heading text-4xl mb-6">Why Choose Electon?</h2>
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
-          <div className="feature p-6 rounded-lg shadow-lg">
-            <div className="feature-icon text-blue-900 mb-4">
-              <i className="fas fa-check-circle fa-3x"></i>
+    <div className="py-20 text-slate-900 text-center bg-slate-50">
+      <div className="mx-auto max-w-screen-xl">
+        <h1 className="text-2xl md:text-5xl font-bold leading-tight  mt-10 mb-32">
+          Why Choose Electon?
+        </h1>
+        <div className="grid grid-cols-1 place-items-center lg:grid-cols-3 mx-auto gap-10">
+          {/* 1 */}
+          <div className="card w-96 h-72 flex flex-col justify-center items-center border-[1px] border-gray-700 ">
+            <TbBrandApple className="text-9xl text-cyan-400" />
+
+            <div className="p-6">
+              <h2 className="text-2xl font-semibold mb-10">Top Brands</h2>
+              <p className="mt-2 text-lg">
+                We offer a curated selection of electronic gadgets from the top
+                brands in the industry.
+              </p>
             </div>
-            <h3 className="feature-heading text-2xl mb-2 font-bold">
-              Top Brands
-            </h3>
-            <p className="feature-description text-lg">
-              We offer a curated selection of electronic gadgets from the top
-              brands in the industry.
-            </p>
           </div>
-          <div className="feature p-6 rounded-lg  shadow-lg">
-            <div className="feature-icon text-blue-900 mb-4">
-              <i className="fas FaTruck fa-3x"></i>
+
+          {/* 2 */}
+          <div className="card w-96 h-72 flex flex-col justify-center items-center border-[1px] border-gray-700">
+            <FaShippingFast className="text-7xl text-cyan-400" />
+            <div className="p-6">
+              <h2 className="text-2xl font-semibold mb-10">Fast Shipping</h2>
+              <p className="mt-2 text-lg">
+                Enjoy speedy and reliable shipping options to get your gadgets
+                delivered in no time.
+              </p>
             </div>
-            <h3 className="feature-heading text-2xl mb-2 font-bold">
-              Fast Shipping
-            </h3>
-            <p className="feature-description text-lg">
-              Enjoy speedy and reliable shipping options to get your gadgets
-              delivered in no time.
-            </p>
           </div>
-          <div className="feature p-6 rounded-lg  shadow-lg">
-            <div className="feature-icon text-blue-900 mb-4">
-              <i className="fas fa-heart fa-3x"></i>
+
+          {/* 3 */}
+          <div className="card w-96 h-72 flex border-[1px] border-gray-700 flex-col justify-center items-center">
+            <BiBadgeCheck className="text-9xl text-cyan-400" />
+            <div className="p-6">
+              <h2 className="text-2xl font-semibold mb-10">
+                Quality Assurance
+              </h2>
+              <p className="mt-2 text-lg">
+                We stand by the quality of our products and ensure that you
+                receive only the best.
+              </p>
             </div>
-            <h3 className="feature-heading text-2xl mb-2 font-bold">
-              Quality Assurance
-            </h3>
-            <p className="feature-description text-lg">
-              We stand by the quality of our products and ensure that you
-              receive only the best.
-            </p>
           </div>
         </div>
       </div>
-    </section>
+    </div>
   );
 };
 
