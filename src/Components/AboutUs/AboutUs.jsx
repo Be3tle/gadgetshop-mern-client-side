77cd974bbaeeded9e4b8550e7deaec677d6a3e50
Abu Ratin Shoeb <aburatinshoaib@gmail.com>
2024-01-06 19:32:40 +0600

    about us img responsive

--- src/Components/AboutUs/AboutUs.jsx
+++ src/Components/AboutUs/AboutUs.jsx
@@ -1,4 +1,4 @@
-import { Link } from "react-router-dom";
+import { Link } from 'react-router-dom';
 
 const AboutUS = () => {
   return (
@@ -6,10 +6,10 @@ const AboutUS = () => {
       <div className="hero-content flex-col lg:flex-row">
         <img
           src="https://img.freepik.com/free-photo/new-smartwatch-balancing-with-hand_23-2150296477.jpg?w=1060&t=st=1704546742~exp=1704547342~hmac=ee3b03939189a2d859093cd6ed0f16cf5413fc793666e185aca8283d5ed22327"
-          className="max-w-sm rounded-lg shadow-2xl"
+          className="lg:max-w-sm max-w-xs rounded-lg shadow-2xl"
         />
         <div>
-          <h1 className="text-5xl font-bold">
+          <h1 className="text-2xl lg:text-5xl font-bold">
             Elevate Your Tech Game with{' '}
             <span className="text-cyan-500">Electon</span>
           </h1>
@@ -18,7 +18,7 @@ const AboutUS = () => {
             excepturi exercitationem quasi. In deleniti eaque aut repudiandae et
             a id nisi.
           </p>
-          <Link to='/login'>
+          <Link to="/login">
             <button className="btn border-0 bg-cyan-500 hover:bg-cyan-700 text-white">
               Get Started
             </button>
