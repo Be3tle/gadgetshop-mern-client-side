f795e430c398645d50947d24ed5d09bb8a0b4f6f
Abu Ratin Shoeb <aburatinshoaib@gmail.com>
2024-01-06 17:19:35 +0600

    navbar made transparent and fixed

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -51,66 +51,64 @@ const Navbar = () => {
   );
 
   return (
-    <div>
-      <div className="navbar max-w-4xl mx-auto">
-        <div className="navbar-start">
-          <div className="dropdown">
-            <label tabIndex={0} className="btn btn-ghost  lg:hidden">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                className="h-5 w-5"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="2"
-                  d="M4 6h16M4 12h8m-8 6h16"
-                />
-              </svg>
-            </label>
-            <ul
-              tabIndex={0}
-              className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-blue-500 rounded-box w-52"
+    <div className="navbar md:fixed z-10 bg-opacity-70 md:bg-opacity-30  bg-white max-w-screen-2xl mx-auto">
+      <div className="navbar-start">
+        <div className="dropdown">
+          <label tabIndex={0} className="btn btn-ghost  lg:hidden">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-5 w-5"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
             >
-              {navLinks}
-            </ul>
-          </div>
-          <img src={logo} className="w-16" alt="" />
-        </div>
-        <div className="navbar-center hidden lg:flex">
-          <ul className="menu menu-horizontal px-1">{navLinks}</ul>
-        </div>
-        <div className="navbar-end">
-          <label tabIndex={0} className="btn btn-ghost btn-circle avatar">
-            <div className="w-10 rounded-full mr-3">
-              <img src={user ? user?.photoURL : emblem} />
-            </div>
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M4 6h16M4 12h8m-8 6h16"
+              />
+            </svg>
           </label>
-          {user ? (
-            <button
-              onClick={handleSignOut}
-              className="btn bg-cyan-500 hover:bg-cyan-700 text-white"
-            >
-              Sign out
-            </button>
-          ) : (
-            <Link to="/login">
-              <button className="btn bg-cyan-500 hover:bg-cyan-700 text-white">
-                Sign in / Sign up
-              </button>
-            </Link>
-          )}
-
-          <input
-            onClick={handleThemeSwitch}
-            type="checkbox"
-            className="toggle ml-3"
-            id="my-toggle"
-          />
+          <ul
+            tabIndex={0}
+            className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-blue-500 rounded-box w-52"
+          >
+            {navLinks}
+          </ul>
         </div>
+        <img src={logo} className="w-16" alt="" />
+      </div>
+      <div className="navbar-center hidden lg:flex">
+        <ul className="menu menu-horizontal px-1">{navLinks}</ul>
+      </div>
+      <div className="navbar-end">
+        <label tabIndex={0} className="btn btn-ghost btn-circle avatar">
+          <div className="w-10 rounded-full mr-3">
+            <img src={user ? user?.photoURL : emblem} />
+          </div>
+        </label>
+        {user ? (
+          <button
+            onClick={handleSignOut}
+            className="btn bg-cyan-500 border-0 hover:bg-cyan-700 text-white"
+          >
+            Sign out
+          </button>
+        ) : (
+          <Link to="/login">
+            <button className="btn border-0 bg-cyan-500 hover:bg-cyan-700 text-white">
+              Sign in
+            </button>
+          </Link>
+        )}
+
+        <input
+          onClick={handleThemeSwitch}
+          type="checkbox"
+          className="toggle ml-3"
+          id="my-toggle"
+        />
       </div>
     </div>
   );
